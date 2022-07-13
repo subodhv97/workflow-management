d6076c09ea86e1c7ea69bb4470d70d47f29a6ad4
Akhilesh <abc@gmail.com>
2022-07-13 16:45:16 +0530

    INitial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { HodComponent } from './hod/hod.component';
+import { FacultyComponent } from './faculty/faculty.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PrincipalComponent,
+    HodComponent,
+    FacultyComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hod/hod.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hod',
+  templateUrl: './hod.component.html',
+  styleUrls: ['./hod.component.css']
+})
+export class HodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
