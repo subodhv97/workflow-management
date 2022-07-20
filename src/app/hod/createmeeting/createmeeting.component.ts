d945204c65e3fb1a66ca4eb268943aa91aeff092
Subodh Verma <vermasubodh6@gmail.com>
2022-07-20 13:34:57 +0530

    file change in HOD meet comp

--- src/app/hod/createmeeting/createmeeting.component.ts
+++ src/app/hod/createmeeting/createmeeting.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createmeeting.component.css']
 })
 export class CreatemeetingComponent implements OnInit {
+  
 
   constructor() { }
 
