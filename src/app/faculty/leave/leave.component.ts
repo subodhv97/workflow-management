561fd8ee8d911554825141df94df15a39637029b
Subodh Verma <vermasubodh6@gmail.com>
2022-07-22 15:29:13 +0530

    faculty comp-leave-notify-QP

--- src/app/faculty/leave/leave.component.ts
+++ src/app/faculty/leave/leave.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-leave',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leave.component.css']
 })
 export class LeaveComponent implements OnInit {
+  list:any[]=[];
+  getLeave(data:NgForm) {
+    console.log(data);
+  }
+  addLeave(item:any) {
+    this.list.push(item);
+    console.log(this.list);
+  }
+
 
   constructor() { }
 
