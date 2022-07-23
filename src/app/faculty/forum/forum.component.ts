cd5787f8013193cbf1b368ca1a08108198c4707a
Subodh Verma <vermasubodh6@gmail.com>
2022-07-24 01:35:01 +0530

    facutly forum updated

--- src/app/faculty/forum/forum.component.ts
+++ src/app/faculty/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forum',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forum.component.css']
 })
 export class ForumComponent implements OnInit {
+  getforum(data: NgForm) {
+    console.log(data.value);
+  }
 
   constructor() { }
 
